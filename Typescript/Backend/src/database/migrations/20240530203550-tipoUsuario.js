c9537b05a36c6548016d176c5fe413f0bca13a3e
Arthur Torres <54257895+NamelessMiojo@users.noreply.github.com>
2024-06-04 22:48:24 -0300

    v0.0.2 - ajust

--- Typescript/Backend/src/database/migrations/20240530203550-tipoUsuario.js
+++ Typescript/Backend/src/database/migrations/20240530203550-tipoUsuario.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('tipo', {
+    await queryInterface.createTable('tipo_usuario', {
       id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
@@ -20,6 +20,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.dropTable('tipo')
+    await queryInterface.dropTable('tipo_usuario')
   },
 };
